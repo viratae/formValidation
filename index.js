7463abbf1e3b7e3c312d09de9c1868a938c460a8
Emily Virata <virataemily@gmail.com>
2026-03-25 14:20:13 -0600

    Added country and password validation

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ const countryError = document.querySelector('#countryError');
 const zipError = document.querySelector('#zipError');
 const passwordError = document.querySelector('#passwordError');
 const confirmPasswordError = document.querySelector('#confirmPasswordError');
-email.addEventListener('blur', () => {
-    email.classList.remove("hide");
-    showEmailError();
-});
+
 email.addEventListener('input', () => {
     showEmailError();
 });
@@ -26,6 +23,7 @@ function showEmailError() {
     }
     else if(email.validity.typeMismatch) {
         message = "Please enter a valid email address";
+        emailError.classList.add("show");
     }
     else {
         message = "";
@@ -35,24 +33,14 @@ function showEmailError() {
     email.setCustomValidity(message);
     emailError.textContent = message;
 }
-form.addEventListener('submit', (e) => {
-    if(!email.checkValidity()) {
-        showEmailError();
-        e.preventDefault();
-    }
-});
 
-zip.addEventListener('blur', () => {
-    zip.classList.remove("hide");
-    showZipError();
-});
 zip.addEventListener('input', () => {
     showZipError();
 });
 function showZipError() {
     zipError.classList.remove("show");
     zip.setCustomValidity("");
-    let messsage = "";
+    let message = "";
     if(zip.validity.valueMissing) {
         message = "Zip Code is required";
         zipError.classList.add("show");
@@ -68,3 +56,96 @@ function showZipError() {
     zip.setCustomValidity(message);
     zipError.textContent = message;
 }
+country.addEventListener('input', () => {
+    showCountryError();
+});
+function showCountryError() {
+    countryError.classList.remove("show");
+    country.setCustomValidity("");
+    let message = "";
+    if(country.validity.valueMissing) {
+        message = "Country is required";
+        countryError.classList.add("show");
+    }
+    else if(country.validity.patternMismatch || country.validity.tooShort) {
+        message = "Please enter a valid country"
+        countryError.classList.add("show");
+    }
+    else {
+        message = "";
+        countryError.classList.remove("show");
+    }
+    country.setCustomValidity(message);
+    countryError.textContent = message;
+}
+password.addEventListener('input', () => {
+    showPasswordError();
+});
+function showPasswordError() {
+    passwordError.classList.remove("show");
+    password.setCustomValidity("");
+    let message = "";
+    if(password.validity.valueMissing) {
+        message = "Password is required"
+        passwordError.classList.add("show");
+    }
+    else if(password.validity.tooShort) {
+        message = "Passwords must be at least 5 characters";
+        passwordError.classList.add("show");
+    }
+    else if(password.validity.tooLong) {
+        message = "Passwords must be 16 characters or less";
+        passwordError.classList.add("show");
+    }
+    else if(password.validity.patternMismatch) {
+        message = "Passwords must have at least 1 capital letter, 1 lowercase letter, 1 number, and 1 special character";
+        console.log("pattern MIS");
+        passwordError.classList.add("show");
+    }
+    else {
+        message = "";
+        passwordError.classList.remove("show");
+    }
+    password.setCustomValidity(message);
+    passwordError.textContent = message;
+}
+confirmPassword.addEventListener('input', () => {
+    showConfirmPasswordError();
+});
+function showConfirmPasswordError() {
+    confirmPasswordError.classList.remove("show");
+    confirmPassword.setCustomValidity("");
+    let message = "";
+    if(confirmPassword.validity.valueMissing) {
+        message = "You must confirm your password";
+        confirmPasswordError.classList.add("show");
+    }
+    else if(confirmPassword.value !== password.value) {
+        message = "Passwords must match";
+        confirmPasswordError.classList.add("show");
+    }
+    else {
+        message = "";
+        confirmPassword.classList.remove("show");
+    }
+    confirmPassword.setCustomValidity(message);
+    confirmPasswordError.textContent = message;
+}
+form.addEventListener('submit', (e) => {
+    if(!email.checkValidity()) {
+        showEmailError();
+        e.preventDefault();
+    }
+    if(!zip.checkValidity()) {
+        showZipError();
+        e.preventDefault();
+    }
+    if(!country.checkValidity()) {
+        showCountryError();
+        e.preventDefault();
+    }
+    if(!password.checkValidity) {
+        showPasswordError();
+        e.preventDefault();
+    }
+});
